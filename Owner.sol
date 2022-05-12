c79285befa245f354e97e45fad4f133853818b8f
Eric Agustian <boku.no.crazy@gmail.com>
2022-05-12 09:12:51 +0700

    Create Owner.sol

---
+++ Owner.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+contract Owner {
+    address payable public ownerDealer;
+
+    constructor(){
+        ownerDealer = msg.sender;
+    }
+}
