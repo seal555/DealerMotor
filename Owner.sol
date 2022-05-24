3301dbf6a919041da81cbd63e344120ed610beaa
Eric Agustian <boku.no.crazy@gmail.com>
2022-05-24 10:19:16 +0700

    Update Owner.sol

--- Owner.sol
+++ Owner.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.7.0;
 
 contract Owner {
-    address payable public ownerDealer;
+    address payable public dealerOwnerAddress;
 
     constructor(){
-        ownerDealer = msg.sender;
+        dealerOwnerAddress = msg.sender;
     }
+
+    // function stringConcate(string memory a, string memory b) internal pure returns (string memory) {
+    //     return string(abi.encodePacked(a, b));
+    // }
 }
