a28de0f9df5eb01734803c44e0058afae6cd8527
Eric Agustian <boku.no.crazy@gmail.com>
2022-05-24 10:19:56 +0700

    Update Sales.sol

--- Sales.sol
+++ Sales.sol
@@ -2,31 +2,81 @@
 pragma solidity ^0.7.0;
 
 contract Sales {
-    enum StatusBookingMotor {YES, NO}
-    StatusBookingMotor bookingMotor;
+    address payable public customerSalesAddress;
 
-    uint biayaBooking = 1000000 wei;
-    uint hargaMotor = 10000000 wei;
+    enum MotorcycleSalesBookingStatus {YES, NO}
+    MotorcycleSalesBookingStatus public bookingUnitStatus;
+
+    uint public bookingFee = 1000000 wei;
+
+    mapping(string => string) public motorCycleType;
+    mapping(string => uint) public motorCycleFullPrice;
+    mapping(string => uint) public motorCycleStock;
 
     constructor(){
-        bookingMotor = StatusBookingMotor.NO;
+        bookingUnitStatus = MotorcycleSalesBookingStatus.NO;
+
+        motorCycleType['M01'] = "Scooter";
+        motorCycleType['M02'] = "Naked Bike";
+        motorCycleType['M03'] = "SuperSport";
+
+        motorCycleFullPrice['M01'] = 10000000 wei;
+        motorCycleFullPrice['M02'] = 15000000 wei;
+        motorCycleFullPrice['M03'] = 20000000 wei;
+
+        motorCycleStock['M01'] = 10;
+        motorCycleStock['M02'] = 10;
+        motorCycleStock['M03'] = 10;
     }
 
-     /*
-        biaya booking sebesar 1.000.000 wei
+    /*
+        booking fee 1.000.000 wei for all type of motorcycle
     */
-    modifier cekBiayaBooking (uint _biaya) {
-        require(_biaya >= biayaBooking, "Uang booking tidak cukup, minimal 1.000.000 WEI");
+    modifier checkTheBookingFee (uint _fee) {
+        require(_fee >= bookingFee, "Insufficient booking fee, minimum of 1.000.000 WEI");
         _;
     }
 
     /*
-        harga motor normal sebesar 10.000.000 WEI
-        harga motor dengan booking sebesar 10.000.000 WEI - 1.000.000 WEI = 9.000.000 WEI
+        check booking status
     */
-    modifier cekHargaMotor (uint _uang) {
-        require((bookingMotor == StatusBookingMotor.YES && _uang >= (hargaMotor - biayaBooking)) 
-        || (bookingMotor == StatusBookingMotor.NO && _uang >= hargaMotor), "Uang Anda tidak cukup untuk membeli motor");
+    modifier checkTheBookingStatus () {
+        require(bookingUnitStatus == MotorcycleSalesBookingStatus.NO, "You already booked a new motorcycle, transaction can't be proceed");
         _;
+    }
+
+    /*
+        check type
+    */
+    modifier checkMotorcycleType (string memory _motorCycleType){
+        require(motorCycleFullPrice[_motorCycleType] != 0, string(abi.encodePacked("Invalid motorcycle type ", _motorCycleType)));
+         _;
+    }
+
+    /*
+        full motorcyle example price is 10.000.000 WEI
+        motorcyle example price with booking fee (10.000.000 WEI - 1.000.000 WEI = 9.000.000 WEI)
+    */
+    modifier checkTheMotorcyclePrice (uint _fee, string memory _motorCycleType) {
+        require((bookingUnitStatus == MotorcycleSalesBookingStatus.YES && _fee >= (motorCycleFullPrice[_motorCycleType] - bookingFee)) 
+        || (bookingUnitStatus == MotorcycleSalesBookingStatus.NO && _fee >= motorCycleFullPrice[_motorCycleType]), "Sorry! Insufficient money to buy a new motorcyle!");
+        _;
+    }
+
+    /*
+        check stock availability
+    */
+    modifier checkMotorcycleStock (string memory _motorCycleType){
+        require(motorCycleStock[_motorCycleType] > 0, string(abi.encodePacked("Sorry the stock of ", motorCycleType[_motorCycleType], " unavailable")));
+         _;
+    }
+
+    /*
+        only customer allowed to do transaction
+    */
+    modifier checkCustomerSalesAddress (address payable _dealerOwnerAddress, address payable _customerSalesAddress){
+     customerSalesAddress = _customerSalesAddress;
+        require(_customerSalesAddress != _dealerOwnerAddress, "Owner unable to transact! Only customer allowed!");
+         _;
     }
 }
